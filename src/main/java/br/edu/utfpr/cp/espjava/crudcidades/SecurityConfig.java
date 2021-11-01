443e06f09ed010799823cbbadffe4fe5a53ba483
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2021-11-01 18:26:52 -0300

    Added authority mapping

--- src/main/java/br/edu/utfpr/cp/espjava/crudcidades/SecurityConfig.java
+++ src/main/java/br/edu/utfpr/cp/espjava/crudcidades/SecurityConfig.java
@@ -1,9 +1,14 @@
 package br.edu.utfpr.cp.espjava.crudcidades;
 
+import java.util.Set;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 
 @EnableWebSecurity
 @Configuration
@@ -13,17 +18,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
             .csrf().disable()
             .authorizeRequests()
-            .antMatchers("/").authenticated()
-            // .antMatchers("/").hasAnyAuthority("listar", "admin")
+            // .antMatchers("/").authenticated()
+            .antMatchers("/").hasAnyAuthority("listar", "admin")
             .antMatchers("/criar").hasAuthority("admin")
             .antMatchers("/excluir").hasAuthority("admin")
             .antMatchers("/preparaAlterar").hasAuthority("admin")
             .antMatchers("/alterar").hasAuthority("admin")
             .anyRequest().denyAll()
                 .and()
-            .oauth2Login().permitAll()
-                .and()
-            .logout().permitAll();
+            .oauth2Login().userInfoEndpoint().userAuthoritiesMapper(userAuthoritiesMapper());
+                
+    }
+
+    @Bean
+     public GrantedAuthoritiesMapper userAuthoritiesMapper() {
+       return (authorities) -> Set.of(new SimpleGrantedAuthority("admin"));
     }
 
 }
